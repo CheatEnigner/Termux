7ce6669dedd41ecc208b6d1a1fa82032a96f5b0f
CheatEnigner <kapixtwitch@gmail.com>
2024-07-07 14:02:05 +0200

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -8,6 +8,7 @@ echo java -Xmx2G -Xms2G -jar spigot-1.19.jar nogui >run.sh
 chmod +x run.sh
 chmod +x spigot-1.19.jar
 mkdir plugins
+cd plugins
 wget https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.0.1/PAPER/ViaVersion-5.0.1.jar
 wget https://hangarcdn.papermc.io/plugins/ViaVersion/ViaBackwards/versions/5.0.1/PAPER/ViaBackwards-5.0.1.jar
 wget https://hangarcdn.papermc.io/plugins/ViaVersion/ViaRewind/versions/4.0.1/PAPER/ViaRewind-4.0.1.jar
